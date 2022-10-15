612526ffead15df2b0888e01efd809db32380756
igopalakrishnan <kingkrishna809@gmail.com>
2022-10-15 19:06:06 +0530

    update navbar, footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Technologies from './Technologies';
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from "../assets/img/nav-icon4.svg";
 
- const Footer = () => {
+const Footer = () => {
     return (
         <footer className="footer">
             <Container>
@@ -16,11 +17,11 @@ import navIcon3 from "../assets/img/nav-icon3.svg";
                     </Col>
                     <Col size={12} sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="#"><img src={navIcon1} alt="Icon" /></a>
-                            <a href="#"><img src={navIcon2} alt="Icon" /></a>
-                            <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                            <a href="https://www.linkedin.com/in/igopalakrishnan" target="_blank"><img src={navIcon1} alt="" /></a>
+                            <a href="https://www.facebook.com/igopalakrishnan" target="_blank"><img src={navIcon2} alt="" /></a>
+                            <a href="https://www.instagram.com/i__gopalakrishnan/" target="_blank"><img src={navIcon3} alt="" /></a>
+                            <a href="https://github.com/igopalakrishnan" target="_blank"><img src={navIcon4} alt="" /></a>
                         </div>
-                        <p>Copyright 2022. All Rights Reserved</p>
                     </Col>
                 </Row>
             </Container>

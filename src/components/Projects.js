375355a34da26ceb82adc80cd95d37689f3bdc30
igopalakrishnan <kingkrishna809@gmail.com>
2022-11-08 18:37:29 +0530

    Add Show more/less in project image

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,128 +16,139 @@ import { ArrowRightCircle } from 'react-bootstrap-icons';
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { useState } from "react";
+import { IoMdArrowDropup, IoMdArrowDropdown } from 'react-icons/io'
+
+const projects = [
+    {
+        title: "Client Portal",
+        imgUrl: Client,
+    },
+    {
+        title: "Course Tracker",
+        imgUrl: Education,
+    },
+    {
+        title: "Portfolio",
+        imgUrl: Portfolio,
+    },
+    {
+        title: "Instagram Clone",
+        imgUrl: Instagram,
+    },
+    {
+        title: "Chat App",
+        imgUrl: ChatApp,
+    },
+    {
+        title: "Book Keeping App",
+        imgUrl: Bookkeeping,
+    },
+    {
+        title: "E-commerce",
+        imgUrl: Ecommerce,
+    },
+    {
+        title: "Deliveroo Clone",
+        imgUrl: Deliveroo,
+    },
+    {
+        title: "Insta Clone",
+        imgUrl: Instaclone,
+    },
+    {
+        title: "Inshort Clone",
+        imgUrl: News,
+    },
+    {
+        title: "Signal Clone",
+        imgUrl: Signal,
+    },
+    {
+        title: "Spotify 2.0",
+        imgUrl: Spotify,
+    },
+    {
+        title: "Disney Clone",
+        imgUrl: Movie,
+    },
+];
+
+const reactjs = [
+    {
+        title: "Client Portal",
+        imgUrl: Client,
+    },
+    {
+        title: "Portfolio",
+        imgUrl: Portfolio,
+    },
+    {
+        title: "Instagram Clone",
+        imgUrl: Instagram,
+    },
+    {
+        title: "Chat App",
+        imgUrl: ChatApp,
+    },
+    {
+        title: "Book Keeping App",
+        imgUrl: Bookkeeping,
+    },
+    {
+        title: "E-commerce",
+        imgUrl: Ecommerce,
+    },
+    {
+        title: "Spotify 2.0",
+        imgUrl: Spotify,
+    },
+    {
+        title: "Disney Clone",
+        imgUrl: Movie,
+    },
+];
+
+const reactnative = [
+    {
+        title: "Deliveroo Clone",
+        imgUrl: Deliveroo,
+    },
+    {
+        title: "Course Tracker",
+        imgUrl: Education,
+    },
+    {
+        title: "Insta Clone",
+        imgUrl: Instaclone,
+    },
+    {
+        title: "Inshort Clone",
+        imgUrl: News,
+    },
+    {
+        title: "Signal Clone",
+        imgUrl: Signal,
+    },
+];
+
+/* const nextjs = [
+    {
+        title: "Spotify 2.0",
+        imgUrl: Spotify,
+    },
+]; */
 
 const Projects = () => {
+    const [itemsToShow, setItemsToShow] = useState(3);
 
-    const projects = [
-        {
-            title: "Client Portal",
-            imgUrl: Client,
-        },
-        {
-            title: "Course Tracker",
-            imgUrl: Education,
-        },
-        {
-            title: "Portfolio",
-            imgUrl: Portfolio,
-        },
-        {
-            title: "Instagram Clone",
-            imgUrl: Instagram,
-        },
-        {
-            title: "Chat App",
-            imgUrl: ChatApp,
-        },
-        {
-            title: "Book Keeping App",
-            imgUrl: Bookkeeping,
-        },
-        {
-            title: "E-commerce",
-            imgUrl: Ecommerce,
-        },
-        {
-            title: "Deliveroo Clone",
-            imgUrl: Deliveroo,
-        },
-        {
-            title: "Insta Clone",
-            imgUrl: Instaclone,
-        },
-        {
-            title: "Inshort Clone",
-            imgUrl: News,
-        },
-        {
-            title: "Signal Clone",
-            imgUrl: Signal,
-        },
-        {
-            title: "Spotify 2.0",
-            imgUrl: Spotify,
-        },
-        {
-            title: "Disney Clone",
-            imgUrl: Movie,
-        },
-    ];
+    const showmore = () => {
+        setItemsToShow(projects.length)
+    }
 
-    const reactjs = [
-        {
-            title: "Client Portal",
-            imgUrl: Client,
-        },
-        {
-            title: "Portfolio",
-            imgUrl: Portfolio,
-        },
-        {
-            title: "Instagram Clone",
-            imgUrl: Instagram,
-        },
-        {
-            title: "Chat App",
-            imgUrl: ChatApp,
-        },
-        {
-            title: "Book Keeping App",
-            imgUrl: Bookkeeping,
-        },
-        {
-            title: "E-commerce",
-            imgUrl: Ecommerce,
-        },
-        {
-            title: "Spotify 2.0",
-            imgUrl: Spotify,
-        },
-        {
-            title: "Disney Clone",
-            imgUrl: Movie,
-        },
-    ];
-
-    const reactnative = [
-        {
-            title: "Deliveroo Clone",
-            imgUrl: Deliveroo,
-        },
-        {
-            title: "Course Tracker",
-            imgUrl: Education,
-        },
-        {
-            title: "Insta Clone",
-            imgUrl: Instaclone,
-        },
-        {
-            title: "Inshort Clone",
-            imgUrl: News,
-        },
-        {
-            title: "Signal Clone",
-            imgUrl: Signal,
-        },
-    ];
-
-    /* const nextjs = [
-        {
-            title: "Spotify 2.0",
-            imgUrl: Spotify,
-        },
-    ]; */
+    const showless = () => {
+        setItemsToShow(3)
+    }
 
     return (
         <section className="project" id="projects">
@@ -160,7 +171,7 @@ const Projects = () => {
                                             <Nav.Item>
                                                 <Nav.Link eventKey="third">React Native</Nav.Link>
                                             </Nav.Item>
-                                           {/*  <Nav.Item>
+                                            {/*  <Nav.Item>
                                                 <Nav.Link eventKey="fourth">Next. JS</Nav.Link>
                                             </Nav.Item> */}
                                         </Nav>
@@ -168,7 +179,7 @@ const Projects = () => {
                                             <Tab.Pane eventKey="first">
                                                 <Row >
                                                     {
-                                                        projects.map((project, index) =>
+                                                        projects.slice(0, itemsToShow).map((project, index) =>
                                                             <Col size={12} sm={6} md={4} key={index}>
                                                                 <div className="proj-imgbx">
                                                                     <img src={project.imgUrl} />
@@ -179,14 +190,15 @@ const Projects = () => {
                                                             </Col>)
 
                                                     }
+
                                                 </Row>
-                                               
+
                                             </Tab.Pane>
-                                           
+
                                             <Tab.Pane eventKey="second">
                                                 <Row >
                                                     {
-                                                        reactjs.map((project, index) =>
+                                                        reactjs.slice(0, itemsToShow).map((project, index) =>
                                                             <Col size={12} sm={6} md={4} key={index}>
                                                                 <div className="proj-imgbx">
                                                                     <img src={project.imgUrl} />
@@ -202,7 +214,7 @@ const Projects = () => {
                                             <Tab.Pane eventKey="third">
                                                 <Row >
                                                     {
-                                                        reactnative.map((project, index) =>
+                                                        reactnative.slice(0, itemsToShow).map((project, index) =>
                                                             <Col size={12} sm={6} md={4} key={index}>
                                                                 <div className="proj-imgbx">
                                                                     <img src={project.imgUrl} />
@@ -232,14 +244,26 @@ const Projects = () => {
                                                     }
                                                 </Row>
                                             </Tab.Pane> */}
+
+                                            {
+                                                (itemsToShow === 3) ?
+                                                    <button className="show-projects mb-5 justify-content-center align-items-center" onClick={showmore}>
+                                                        Show More
+                                                        <IoMdArrowDropdown />
+                                                    </button>
+                                                    : <button className="show-projects mb-5 justify-content-center align-items-center" onClick={showless}>
+                                                        Show Less
+                                                        <IoMdArrowDropup />
+                                                    </button>
+                                            }
                                             <button>
-                                                    <a style={{
-                                                        textDecoration: "none",
-                                                        color: "#fff"
-                                                    }} href="https://igopalakrishnan.github.io/Profile-Projects" target="_blank">More Info
-                                                        <ArrowRightCircle size={27} />
-                                                    </a>
-                                                </button>
+                                                <a style={{
+                                                    textDecoration: "none",
+                                                    color: "#fff"
+                                                }} href="https://igopalakrishnan.github.io/Profile-Projects" target="_blank">More Info
+                                                    <ArrowRightCircle size={27} />
+                                                </a>
+                                            </button>
                                         </Tab.Content>
                                     </Tab.Container>
                                 </div>}

126ef31fa336e1aad754dadf3d706bc6f4f53f5b
gopalakrishnan <kingkrishna809@gmail.com>
2026-05-18 11:44:56 +0530

    Add logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link as ScrollLink } from "react-scroll"; // for smooth section scroll
 import "./Navbar.css";
+import Logo from "./Logo.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,35 +18,112 @@ const Navbar = () => {
   return (
     <>
       <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
-        <div className="logo">GK</div>
+        <div className="logo">
+          <img src={Logo} alt="GK Logo" />
+        </div>
         <div className="desktop-links">
           {/* Smooth scroll links */}
-          <ScrollLink to="home" smooth={true} duration={600} onClick={() => setIsOpen(false)}>Home</ScrollLink>
-          <ScrollLink to="about" smooth={true} duration={600} onClick={() => setIsOpen(false)}>About</ScrollLink>
-          <ScrollLink to="skills" smooth={true} duration={600} onClick={() => setIsOpen(false)}>Skills</ScrollLink>
-          <ScrollLink to="projects" smooth={true} duration={600} onClick={() => setIsOpen(false)}>Projects</ScrollLink>
-          <ScrollLink to="getInTouch" smooth={true} duration={600} onClick={() => setIsOpen(false)} className="pill-btn">
-             Get In Touch
+          <ScrollLink
+            to="home"
+            smooth={true}
+            duration={600}
+            onClick={() => setIsOpen(false)}
+          >
+            Home
+          </ScrollLink>
+          <ScrollLink
+            to="about"
+            smooth={true}
+            duration={600}
+            onClick={() => setIsOpen(false)}
+          >
+            About
+          </ScrollLink>
+          <ScrollLink
+            to="skills"
+            smooth={true}
+            duration={600}
+            onClick={() => setIsOpen(false)}
+          >
+            Skills
+          </ScrollLink>
+          <ScrollLink
+            to="projects"
+            smooth={true}
+            duration={600}
+            onClick={() => setIsOpen(false)}
+          >
+            Projects
+          </ScrollLink>
+          <ScrollLink
+            to="getInTouch"
+            smooth={true}
+            duration={600}
+            onClick={() => setIsOpen(false)}
+            className="pill-btn"
+          >
+            Get In Touch
           </ScrollLink>
         </div>
 
         {!isOpen && (
           <div className="hamburger" onClick={toggleMenu}>
-            <span className="bar"></span><span className="bar"></span><span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
           </div>
         )}
       </nav>
 
       {/* Mobile menu */}
       <div className={`nav-links ${isOpen ? "open" : ""}`}>
-        <div className={`hamburger ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
-          <span className="bar"></span><span className="bar"></span><span className="bar"></span>
+        <div
+          className={`hamburger ${isOpen ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
         </div>
-        <ScrollLink to="home" smooth={true} duration={600} onClick={() => setIsOpen(false)}>Home</ScrollLink>
-        <ScrollLink to="about" smooth={true} duration={600} onClick={() => setIsOpen(false)}>About</ScrollLink>
-        <ScrollLink to="skills" smooth={true} duration={600} onClick={() => setIsOpen(false)}>Skills</ScrollLink>
-        <ScrollLink to="projects" smooth={true} duration={600} onClick={() => setIsOpen(false)}>Projects</ScrollLink>
-        <ScrollLink to="getInTouch" smooth={true} duration={600} onClick={() => setIsOpen(false)} className="pill-btn">
+        <ScrollLink
+          to="home"
+          smooth={true}
+          duration={600}
+          onClick={() => setIsOpen(false)}
+        >
+          Home
+        </ScrollLink>
+        <ScrollLink
+          to="about"
+          smooth={true}
+          duration={600}
+          onClick={() => setIsOpen(false)}
+        >
+          About
+        </ScrollLink>
+        <ScrollLink
+          to="skills"
+          smooth={true}
+          duration={600}
+          onClick={() => setIsOpen(false)}
+        >
+          Skills
+        </ScrollLink>
+        <ScrollLink
+          to="projects"
+          smooth={true}
+          duration={600}
+          onClick={() => setIsOpen(false)}
+        >
+          Projects
+        </ScrollLink>
+        <ScrollLink
+          to="getInTouch"
+          smooth={true}
+          duration={600}
+          onClick={() => setIsOpen(false)}
+          className="pill-btn"
+        >
           Get In Touch
         </ScrollLink>
       </div>
